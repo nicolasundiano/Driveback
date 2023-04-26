2b89c6894073db22f88e55819962232f1b7626aa
nicolas <nicolasundiano@gmail.com>
2023-04-26 20:24:46 -0300

    fix

--- src/Core/Domain/Users/Entities/ChildUser.cs
+++ src/Core/Domain/Users/Entities/ChildUser.cs
@@ -8,7 +8,7 @@ public class ChildUser : Entity<Guid>
     public string Property1 { get; private set; }
     public int Property2 { get; private set; }
 
-    private ChildUser(Guid id, string property1, int property2)
+    private ChildUser(Guid id, string property1, int property2) : base(id)
     {
         Property1 = property1;
         Property2 = property2;
@@ -21,9 +21,23 @@ public class ChildUser : Entity<Guid>
         return new ChildUser(Guid.NewGuid(), property1, property2);
     }
 
+    public void Update(string property1, int property2)
+    {
+        Property1 = property1;
+        Property2 = property2;
+        
+        Validate();
+    }
+
     private void Validate()
     {
         ValidationHelper.ValidateString(Property1, nameof(Property1), 100);
         ValidationHelper.ValidateOutOfRange(Property2, nameof(Property2), 1, 500);
     }
+    
+#pragma warning disable CS8618
+    private ChildUser()
+    {
+    }
+#pragma warning restore CS8618
 }
