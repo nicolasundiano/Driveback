007ebaeb1723de243f56a58186448fd7f13e0df1
nicolas <nicolasundiano@gmail.com>
2023-04-26 20:24:23 -0300

    refactor

--- src/Infrastructure/Persistence/Repositories/EfReadRepository.cs
+++ src/Infrastructure/Persistence/Repositories/EfReadRepository.cs
@@ -19,35 +19,30 @@ internal class EfReadRepository<TEntity> : IReadRepository<TEntity> where TEntit
     public async Task<TEntity?> GetAsync(ISpecification<TEntity> spec, CancellationToken cancellationToken = default)
     {
         return await ApplySpecification(spec)
-            .AsNoTracking()
             .FirstOrDefaultAsync(cancellationToken);
     }
 
     public async Task<List<TEntity>> ListAsync(CancellationToken cancellationToken = default)
     {
         return await _context.Set<TEntity>()
-            .AsNoTracking()
             .ToListAsync(cancellationToken);
     }
 
     public async Task<List<TEntity>> ListAsync(ISpecification<TEntity> spec, CancellationToken cancellationToken = default)
     {
         return await ApplySpecification(spec)
-            .AsNoTracking()
             .ToListAsync(cancellationToken);
     }
 
     public async Task<int> CountAsync(CancellationToken cancellationToken = default)
     {
         return await _context.Set<TEntity>()
-            .AsNoTracking()
             .CountAsync(cancellationToken);
     }
 
     public async Task<int> CountAsync(ISpecification<TEntity> spec, CancellationToken cancellationToken = default)
     {
         return await ApplySpecification(spec, true)
-            .AsNoTracking()
             .CountAsync(cancellationToken);
     }
     
@@ -56,6 +51,6 @@ internal class EfReadRepository<TEntity> : IReadRepository<TEntity> where TEntit
         return SpecificationEvaluator<TEntity>.GetQuery(
             _context.Set<TEntity>().AsQueryable(),
             spec,
-            evaluateOnlyCriteria);
+            evaluateOnlyCriteria).AsNoTracking();
     }
 }
